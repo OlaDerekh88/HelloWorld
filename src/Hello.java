a81e2d64abca6377cb8e29e443cb1766b57089a9
Olga Derekh <olga.der88@gmail.com>
2025-04-25 21:09:14 +0300

    09.Logical AND Operator

--- src/Hello.java
+++ src/Hello.java
@@ -14,5 +14,33 @@ public class Hello {
             System.out.println("It is not an alien!");
             System.out.println("And I am scared of aliens");
         }
+
+//        int topScore = 100;
+//        if (topScore == 100) {
+//            System.out.println("You got the high score!");
+//        }
+
+        //ne riwnyj 100, not = 100 use !=
+//        int topScore = 100;
+//        if (topScore != 100) {
+//            System.out.println("You got the high score!");
+//        }
+
+    // jakszo umowa >=100 printyje You....
+//        int topScore = 100;
+//        if (topScore >= 100) {
+//            System.out.println("You got the high score!");
+//        }
+
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You got the high score!");
+        }
+
+        // && - i, oznacza e 2 warunki maja byc spelnione
+        int secondTopScore = 60;
+        if (topScore > secondTopScore && topScore < 100) {
+            System.out.println("Greater than second top score and less than 100");
+        }
     }
 }
